f8f78d85435e47b44c7079fc9f94897d03fe1884
BMA Dev Team <dev@bmaphotography.co.ke>
2026-05-13 11:46:55 +0000

    fix: push content slightly down with increased top padding

--- components/hero.tsx
+++ components/hero.tsx
@@ -66,8 +66,8 @@ export function Hero() {
       {/* Content Container */}
       <div className="relative z-10 flex-1 flex flex-col">
         
-        {/* Main Content Area - Pushed up near header */}
-        <div className="pt-16 sm:pt-14 pb-4">
+        {/* Main Content Area - Slightly down from top */}
+        <div className="pt-24 sm:pt-28 pb-4">
           <div className="mx-auto max-w-7xl w-full px-4 sm:px-6 lg:px-8">
             <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-6 lg:gap-12">
               
