78f15b1a1538168b32eafeaf7937f3cac642428c
BMA Dev Team <dev@bmaphotography.co.ke>
2026-05-13 11:05:21 +0000

    fix: remove justify-center, reduce padding to push content up significantly

--- components/hero.tsx
+++ components/hero.tsx
@@ -64,10 +64,10 @@ export function Hero() {
       </div>
 
       {/* Content Container */}
-      <div className="relative z-10 flex-1 flex flex-col justify-center">
+      <div className="relative z-10 flex-1 flex flex-col">
         
-        {/* Main Content Area - No flex-1 to prevent pushing stats down */}
-        <div className="pt-20 sm:pt-16 pb-4">
+        {/* Main Content Area - Pushed up near header */}
+        <div className="pt-16 sm:pt-14 pb-4">
           <div className="mx-auto max-w-7xl w-full px-4 sm:px-6 lg:px-8">
             <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-6 lg:gap-12">
               
